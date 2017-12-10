8739e1d3b3f85361340f8a591640fa19bba11060
Adam Hutchison <ajhutchi@kent.edu>
2017-12-09 23:40:52 -0500

    Fixed babel issues

--- webpack.config.js
+++ webpack.config.js
@@ -15,14 +15,12 @@ module.exports = {
 	module: {
 		loaders: [
 			{
-				test: /\.js$/,
-				loader: 'babel-loader',
-				exclude: /node_modules/
-			},
-			{
 				test: /\.jsx?$/,
+				exclude: /node_modules/,
 				loader: 'babel-loader',
-				exclude: /node_modules/
+				query: {
+					presets: ['react']
+				}
 			}
 		]
 	},
